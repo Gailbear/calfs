ced43b52e37ab9fb676f7d86e4e8b3b111f723f6
Gailbear <gterman@gmail.com>
2012-10-31 17:28:56 -0400

    fixed double indirect problems.

--- 3600fs.h
+++ 3600fs.h
@@ -9,14 +9,15 @@
 #define __3600FS_H__
 
 static int getattr_from_direntry(direntry target_d, struct stat *stbuf);
-static direntry findFile(const char *path);
-static direntry findfile_indirect(blocknum block, const char *path);
-static direntry findfile_dindirect(blocknum block, const char *path);
+static direntry findFile(const char *path, blocknum *dntnum, int *entry);
+static direntry findfile_indirect(blocknum block, const char *path, blocknum *dntnum, int *entry);
+static direntry findfile_dindirect(blocknum block, const char *path, blocknum *dntnum, int *entry);
 static int readdir_indirect(blocknum block, void *buf, fuse_fill_dir_t filler, off_t offset);
 static int readdir_dindirect(blocknum block, void *buf, fuse_fill_dir_t filler, off_t offset);
 //static blocknum startFindPath(direntry startingDir, const char *path);
 //static blocknum findPath(const char *path);
 static void load_root();
+static void free_block(blocknum b);
 
 
 #endif
